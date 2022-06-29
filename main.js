9ba3ea6a0e16bcf8cc70e7544fc247c721f91b46
mansimishra123 <95296459+mansimishra123@users.noreply.github.com>
2022-06-29 07:57:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-function start{
+status=""
 
+function start(){
     
+        objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+        document.getElementById("status").innerHTML="Status: detecting objects "
+        input_text=document.getElementById("input_id").value
+    
+    }
+    
+
+
+function modelLoaded(){
+
+    console.log("Model Loaded !")
+    status=true;
+    }
+
+function setup(){
+    canvas= createCanvas(300,290)
+    canvas.position(480,250)
+
+    video= createCapture(VIDEO)
+
+    video.size(300,290)
+    video.hide()
+
 }
+
+function draw(){
+
+    image(video,0,0,300,290)
+}
+
