b68657800345e18f1952384ad730442957e45e76
mansimishra123 <95296459+mansimishra123@users.noreply.github.com>
2022-06-29 17:57:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 status=""
+object=[]
 
-function start(){
-    
-        objectDetector=ml5.objectDetector("cocossd",modelLoaded)
-        document.getElementById("status").innerHTML="Status: detecting objects "
-        input_text=document.getElementById("input_id").value
-    
-    }
-    
+function setup(){
+    canvas= createCanvas(380,380)
+    canvas.center()
 
+    video= createCapture(VIDEO)
+
+    video.size(380,380)
+    video.hide()
+
+}
 
 function modelLoaded(){
 
@@ -16,19 +18,58 @@ function modelLoaded(){
     status=true;
     }
 
-function setup(){
-    canvas= createCanvas(300,290)
-    canvas.position(480,250)
 
-    video= createCapture(VIDEO)
-
-    video.size(300,290)
-    video.hide()
-
-}
+function begin(){
+    
+        objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+        document.getElementById("status").innerHTML="Status: detecting objects "
+        input_text=document.getElementById("input_id").value
+    
+    }
 
 function draw(){
 
-    image(video,0,0,300,290)
+    image(video,0,0,380,380)
+    if(status !=""){
+        objectDetector.detect(video,gotResult)
+        for(i=0;i<object.length;i++){
+
+            document.getElementById("status").innerHTML="Status:objects detected "
+                        fill("red")
+                        precent=floor(object[i].confidence*100)
+                        text(object[i].label+""+precent+"%",object[i].x,object[i].y)
+                        noFill()
+                        stroke("red")
+                        rect(object[i].x,object[i].y,object[i].width,object[i].height)
+
+
+                        if(object[i].label==input_text){
+
+                            video.stop()
+
+                            objectDetector.detect(gotResult)
+                            document.getElementById("object_found").innerHTML=input_text+"found"
+
+                            synth=window.speechSynthesis
+                            utterthis=new SpeechSynthesisUtterance(input_text+"found")
+                            synth.speak(utterthis)
+                        }
+
+                        else{
+                            document.getElementById("object_found").innerHTML=input_text+"not found"
+
+                        }
+        }
+    }
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log(error)
+    }
+
+    console.log(result)
+
+    object=result
 }
 
